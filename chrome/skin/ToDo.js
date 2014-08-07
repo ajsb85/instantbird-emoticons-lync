2f50f1620b172a7867fb01ce8867a2f287c255d4
Alexander Salas <a.salas@ieee.org>
2014-08-07 14:51:27 -0430

    pre-release
    need add more emoticons

---
+++ chrome/skin/ToDo.js
@@ -0,0 +1,109 @@
+
+/* 
+		{"filename": "PeopleAndFaces1.png", 		"textCodes":	[":'("]},
+		{"filename": "PeopleAndFaces1.png", 		"textCodes":	["^_^"]},
+		{"filename": "PeopleAndFaces1.png", 		"textCodes":	[":-|", ":|"]},
+		{"filename": "PeopleAndFaces1.png", 		"textCodes":	["-_-"]},
+		{"filename": "PeopleAndFaces1.png", 		"textCodes":	[">_<"]},
+		{"filename": "PeopleAndFaces1.png", 		"textCodes":	[">:-(", ">:("]},
+		{"filename": "PeopleAndFaces1.png", 		"textCodes":	["X-(", "X("]},
+		{"filename": "PeopleAndFaces1.png", 		"textCodes":	[":-/", ":/", "=/"]},
+		{"filename": "PeopleAndFaces1.png", 		"textCodes":	[":*"]},
+		{"filename": "PeopleAndFaces1.png", 		"textCodes":	[":-O", ":O"]},
+		{"filename": "PeopleAndFaces1.png", 		"textCodes":	["O:-)", "O:)"]},
+		{"filename": "PeopleAndFaces1.png", 		"textCodes":	["}:-)"]},
+		{"filename": "PeopleAndFaces1.png", 		"textCodes":	["<3"]},
+		{"filename": "PeopleAndFaces1.png", 		"textCodes":	["</3"]},
+		{"filename": "PeopleAndFaces1.png", 		"textCodes":	[":(|)"]},
+		{"filename": "PeopleAndFaces1.png", 		"textCodes":	[":(:)"]},
+		{"filename": "PeopleAndFaces1.png", 		"textCodes":	["~@~"]}
+
+
+ROW ONE:
+
+Smiley :)
+Open mouthed smiley  :>
+Winking smiley ;)
+Surprised smiley :-o
+Smiley with tongue out :p
+Hot smiley (sunglasses) (h)
+Angry smiley :@
+Embarrassed smiley :$
+Confused smiley :s
+Sad smiley :<
+ROW TWO:
+
+Crying face :’(
+Disappointed Smiley :|
+Bearing teeth smiley 8o|
+Sarcastic smiley ^o)
+Sick smiley +o(
+Thinking smiley *-)
+Eye-rolling smiley 8-)
+Sleepy smiley |-)
+Boy (z)
+Girl (x)
+
+ROW THREE:
+
+Sun (#)
+Sleeping half-moon (s)
+Star (*)
+Rainbow (r)
+Storm cloud (st)
+Umbrella (um)
+Island with palm tree (ip)
+Auto (au)
+Airplane (ap)
+Gotta run (gtr)
+
+ROW FOUR:
+
+Red lips (k)
+Red rose (f)
+Red heart (l)
+Broken heart (u)
+Filmstrip (~)
+Music Note (8)
+Camera (p)
+Party smiley <:o)
+Gift box with bow (g)
+Birthday cake (^)
+
+ROW FIVE:
+
+Pizza (pi)
+Drink (d)
+Coffee cup (c)
+Beer (b)
+Soccer ball (so)
+Games (ply)
+Money (mo)
+Good luck (gl)
+I agree/ check mark (y)
+I disagree/ red X mark (n)
+
+ROW SIX:
+
+Cat face (@)
+Dog face (&)
+Snail (sn)
+Black sheep (bah)
+Working from home (@h)
+Working on it (woi)
+Computer (co)
+Mobile phone (mp)
+Telephone receiver (t)
+Email (e)
+
+ROW SEVEN:
+
+Can you talk/ Quotation icon: (!!)
+Clock (o)
+Hold on (w8)
+What’s going on (!!?)
+Light bulb (i)
+Online green icon (ol)
+Busy icon (busy)
+Do Not Disturb icon (dnd)
+Confidential/ lock & key (qt) */
---
+++ chrome/skin/theme.js
@@ -0,0 +1,12 @@
+﻿{
+	"smileys":
+	[
+		{"filename": "PeopleAndFaces1.png", 		"textCodes":	[":-)", ":)", "=)"]},
+		{"filename": "PeopleAndFaces0.png", 		"textCodes":	[":-D", ":D", ":>", ":->"]},
+		{"filename": "PeopleAndFaces3.png", 		"textCodes":	[";-)", ";)"]},
+		{"filename": "PeopleAndFaces38.png", 		"textCodes":	[":-o", ":o"]},
+		{"filename": "PeopleAndFaces23.png", 		"textCodes":	[":-P", ":P"]},
+		{"filename": "PeopleAndFaces14.png", 		"textCodes":	["(sunglasses)", "(h)"]},
+		{"filename": "PeopleAndFaces20.png", 		"textCodes":	[":@"]}
+	]
+}
